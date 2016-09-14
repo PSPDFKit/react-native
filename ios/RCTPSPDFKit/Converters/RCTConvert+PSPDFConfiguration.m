1be7a342364e6aac2bee82f4759933409cd399c3
Robert Wijas <robert.wijas@gmail.com>
2016-09-14 11:49:41 +0200

    Adds proper copyright comments and license.

--- ios/RCTPSPDFKit/Converters/RCTConvert+PSPDFConfiguration.m
+++ ios/RCTPSPDFKit/Converters/RCTConvert+PSPDFConfiguration.m
@@ -1,9 +1,13 @@
 //
 //  RCTConvert+PSPDFConfiguration.m
-//  RCTPSPDFKit
+//  PSPDFKit
 //
-//  Created by Robert Wijas on 12/09/16.
-//  Copyright © 2016 Facebook. All rights reserved.
+//  Copyright (c) 2016 PSPDFKit GmbH. All rights reserved.
+//
+//  THIS SOURCE CODE AND ANY ACCOMPANYING DOCUMENTATION ARE PROTECTED BY INTERNATIONAL COPYRIGHT LAW
+//  AND MAY NOT BE RESOLD OR REDISTRIBUTED. USAGE IS BOUND TO THE PSPDFKIT LICENSE AGREEMENT.
+//  UNAUTHORIZED REPRODUCTION OR DISTRIBUTION IS SUBJECT TO CIVIL AND CRIMINAL PENALTIES.
+//  This notice may not be removed from this file.
 //
 
 #import "RCTConvert+PSPDFConfiguration.h"
--- samples/Catalog/index.android.js
+++ samples/Catalog/index.android.js
@@ -1,8 +1,14 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+//
+//  index.android.js
+//  PSPDFKit
+//
+//  Copyright (c) 2016 PSPDFKit GmbH. All rights reserved.
+//
+//  THIS SOURCE CODE AND ANY ACCOMPANYING DOCUMENTATION ARE PROTECTED BY INTERNATIONAL COPYRIGHT LAW
+//  AND MAY NOT BE RESOLD OR REDISTRIBUTED. USAGE IS BOUND TO THE PSPDFKIT LICENSE AGREEMENT.
+//  UNAUTHORIZED REPRODUCTION OR DISTRIBUTION IS SUBJECT TO CIVIL AND CRIMINAL PENALTIES.
+//  This notice may not be removed from this file.
+//
 
 import React, { Component } from 'react';
 import {
