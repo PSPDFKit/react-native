d79a5400041420639c8a0abb0735cc53bdfe56de
Simone Arpe <simon.arpe@gmail.com>
2016-11-09 10:36:53 +0100

    Fix import

--- samples/Catalog/android/app/src/main/java/com/pspdfkit/react/catalog/MainApplication.java
+++ samples/Catalog/android/app/src/main/java/com/pspdfkit/react/catalog/MainApplication.java
@@ -6,7 +6,7 @@ import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
-import com.react.pspdfkit.PSPDFKitPackage;
+import com.pspdfkit.react.PSPDFKitPackage;
 
 import java.util.Arrays;
 import java.util.List;
