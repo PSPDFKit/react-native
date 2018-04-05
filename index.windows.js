5044cbf4c8dd2f58a6d1df344b484d101255e195
Nick <nfxdevelopment@gmail.com>
2018-04-05 15:31:32 -0700

    Having to use auto detection of platforms to specify JS

---
+++ index.windows.js
@@ -0,0 +1,10 @@
+//  Copyright © 2018 PSPDFKit GmbH. All rights reserved.
+//
+//  THIS SOURCE CODE AND ANY ACCOMPANYING DOCUMENTATION ARE PROTECTED BY INTERNATIONAL COPYRIGHT LAW
+//  AND MAY NOT BE RESOLD OR REDISTRIBUTED. USAGE IS BOUND TO THE PSPDFKIT LICENSE AGREEMENT.
+//  UNAUTHORIZED REPRODUCTION OR DISTRIBUTION IS SUBJECT TO CIVIL AND CRIMINAL PENALTIES.
+//  This notice may not be removed from this file.
+//
+import { requireNativeComponent } from "react-native";
+
+module.exports = requireNativeComponent("ReactPSPDFKitView", null);
