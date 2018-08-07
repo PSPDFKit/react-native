c18d33b57d9b4992f71450d7dca03c5885623bcc
RadAzzouz <rad@pspdfkit.com>
2018-08-07 11:36:42 -0400

    Fix `ProgrammaticFormFilling` example

--- samples/Catalog/Catalog.ios.js
+++ samples/Catalog/Catalog.ios.js
@@ -559,18 +559,18 @@ class ProgrammaticFormFilling extends Component {
           <View>
             <Button onPress={() => {
               // Fill Text Form Fields.
-              this.refs.pdfView.setFormFieldValue('Name_Last', 'Appleseed');
-              this.refs.pdfView.setFormFieldValue('Name_First', 'John');
-              this.refs.pdfView.setFormFieldValue('Address_1', '1 Infinite Loop');
-              this.refs.pdfView.setFormFieldValue('City', 'Cupertino');
-              this.refs.pdfView.setFormFieldValue('STATE', 'CA');
-              this.refs.pdfView.setFormFieldValue('SSN', '123456789');
-              this.refs.pdfView.setFormFieldValue('Telephone_Home', '(123) 456-7890');
-              this.refs.pdfView.setFormFieldValue('Birthdate', '1/1/1983');
+              this.refs.pdfView.setFormFieldValue('Appleseed', 'Name_Last');
+              this.refs.pdfView.setFormFieldValue('John', 'Name_First');
+              this.refs.pdfView.setFormFieldValue('1 Infinite Loop', 'Address_1');
+              this.refs.pdfView.setFormFieldValue('Cupertino', 'City');
+              this.refs.pdfView.setFormFieldValue('CA', 'STATE');
+              this.refs.pdfView.setFormFieldValue('123456789', 'SSN');
+              this.refs.pdfView.setFormFieldValue('(123) 456-7890', 'Telephone_Home');
+              this.refs.pdfView.setFormFieldValue('1/1/1983', 'Birthdate');
 
               // Select a button form elements.
-              this.refs.pdfView.setFormFieldValue('Sex.0', 'selected');
-              this.refs.pdfView.setFormFieldValue('PHD', 'selected');
+              this.refs.pdfView.setFormFieldValue('selected', 'Sex.0');
+              this.refs.pdfView.setFormFieldValue('selected', 'PHD');
             }} title="Fill Forms" />
           </View>
           <View>
