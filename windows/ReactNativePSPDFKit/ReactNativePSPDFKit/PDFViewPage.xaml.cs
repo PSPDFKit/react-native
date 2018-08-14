98d45d45d775cda1af1e68bfd6010c60cb219163
Nick Winder <nfxdevelopment@gmail.com>
2018-08-14 14:32:01 -0500

    UWP issue when no document is passed. (#114)
    As a response to https://github.com/PSPDFKit/react-native/issues/112 an issue was found when not passing document from JS. I believe this should be an optional feature therefore this PR fixes this. 
    
    No document will attempt to load by default if document is not present. 
    
    In addition to this, some library alignment were made and copyrights updated.

--- windows/ReactNativePSPDFKit/ReactNativePSPDFKit/PDFViewPage.xaml.cs
+++ windows/ReactNativePSPDFKit/ReactNativePSPDFKit/PDFViewPage.xaml.cs
@@ -1,4 +1,13 @@
-﻿using System;
+﻿//
+//  Copyright © 2018 PSPDFKit GmbH. All rights reserved.
+//
+//  THIS SOURCE CODE AND ANY ACCOMPANYING DOCUMENTATION ARE PROTECTED BY INTERNATIONAL COPYRIGHT LAW
+//  AND MAY NOT BE RESOLD OR REDISTRIBUTED. USAGE IS BOUND TO THE PSPDFKIT LICENSE AGREEMENT.
+//  UNAUTHORIZED REPRODUCTION OR DISTRIBUTION IS SUBJECT TO CIVIL AND CRIMINAL PENALTIES.
+//  This notice may not be removed from this file.
+//
+
+using System;
 using System.Threading.Tasks;
 using PSPDFKit.Document;
 using PSPDFKit.UI;
@@ -6,21 +15,17 @@ using Windows.Storage;
 using Windows.UI.Popups;
 using Windows.UI.Xaml.Controls;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
-
 namespace ReactNativePSPDFKit
 {
     public sealed partial class PDFViewPage : Page
     {
-        private ViewState _viewStateCache = new ViewState();
+        private readonly ViewState _viewStateCache = new ViewState();
         private StorageFile _fileToOpen = null;
         private bool _pdfViewInitialised = false;
         
         public PDFViewPage()
         {
             InitializeComponent();
-
-            PDFView.InitializationCompletedHandler += PDFView_InitializationCompletedHandlerAsync;
         }
 
         /// <summary>
@@ -31,11 +36,12 @@ namespace ReactNativePSPDFKit
         {
             _fileToOpen = file;
 
+            // If the PdfView is already initialised we can show the new document.
             if(_pdfViewInitialised)
             {
                 try
                 {
-                    await PDFView.Controller.ShowDocumentAsync(DocumentSource.CreateFromStorageFile(file));
+                    await PDFView.Controller.ShowDocumentWithViewStateAsync(DocumentSource.CreateFromStorageFile(file), _viewStateCache);
                 }
                 catch (Exception e)
                 {
@@ -50,6 +56,7 @@ namespace ReactNativePSPDFKit
         {
             _viewStateCache.CurrentPageIndex = index;
 
+            // If the PdfView is already initialised we can change the page index.
             if (_pdfViewInitialised)
             {
                 await PDFView.Controller.SetCurrentPageIndexAsync(index);
@@ -63,9 +70,13 @@ namespace ReactNativePSPDFKit
             PDFView.ShowToolbar = showToolbar;
         }
 
-        private async void PDFView_InitializationCompletedHandlerAsync(PdfView sender, PSPDFKit.Pdf.Document args)
+        private async void PDFView_InitializationCompletedHandlerAsync(PdfView sender, PSPDFKit.Pdf.Document document)
         {
-            await PDFView.Controller.ShowDocumentWithViewStateAsync(DocumentSource.CreateFromStorageFile(_fileToOpen), _viewStateCache);
+            // If we already have a file to open lets proceed with that here.
+            if (_fileToOpen != null)
+            {
+                await PDFView.Controller.ShowDocumentWithViewStateAsync(DocumentSource.CreateFromStorageFile(_fileToOpen), _viewStateCache);
+            }
             _pdfViewInitialised = true;
         }
     }
