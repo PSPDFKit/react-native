38af66c2e70b884d1cb8b8e4a555709ac58e9a39
Nick <nfxdevelopment@gmail.com>
2019-02-16 10:54:53 +0000

    Added a promise on Present.

--- windows/ReactNativePSPDFKit/ReactNativePSPDFKit/LibraryModule.cs
+++ windows/ReactNativePSPDFKit/ReactNativePSPDFKit/LibraryModule.cs
@@ -89,9 +89,9 @@ namespace ReactNativePSPDFKit
 
             try
             {
-                var storageFolder = await StorageFolder.GetFolderFromPathAsync(path).AsTask().ConfigureAwait(false); ;
+                var storageFolder = await StorageFolder.GetFolderFromPathAsync(path).AsTask().ConfigureAwait(false);
                 // Queue up the PDFs in the folder for indexing.
-                await EnqueueDocuments(_libraries[libraryName], storageFolder).ConfigureAwait(false); ;
+                await EnqueueDocuments(_libraries[libraryName], storageFolder).ConfigureAwait(false);
 
                 promise.Resolve(null);
             }
@@ -131,7 +131,7 @@ namespace ReactNativePSPDFKit
                 }).ConfigureAwait(false); ;
 
                 // Queue up the PDFs in the folder for indexing.
-                await EnqueueDocuments(_libraries[libraryName], folder).ConfigureAwait(false); ;
+                await EnqueueDocuments(_libraries[libraryName], folder).ConfigureAwait(false);
 
                 promise.Resolve(null);
             }
