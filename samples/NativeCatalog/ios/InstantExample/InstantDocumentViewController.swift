6c1fb4e2b564736f46ffaa96890f666643466877
Rad Azzouz <radazzouz@users.noreply.github.com>
2021-01-11 10:26:46 -0500

    Update copyright year to 2021 (#386)
    * Update copyright year to 2021
    
    * Adds copyright header to existing files that did not have one already.
    
    * Adds file to enable ignoring commits from blame.
    
    * Add newline at the end of git-blame-ignore-revs file.
    
    * Fixes minor typos.
    
    Co-authored-by: Nishant Desai <nishdesai95@gmail.com>

--- samples/NativeCatalog/ios/InstantExample/InstantDocumentViewController.swift
+++ samples/NativeCatalog/ios/InstantExample/InstantDocumentViewController.swift
@@ -1,5 +1,5 @@
 //
-//  Copyright © 2019-2020 PSPDFKit GmbH. All rights reserved.
+//  Copyright © 2019-2021 PSPDFKit GmbH. All rights reserved.
 //
 //  The PSPDFKit Sample applications are licensed with a modified BSD license.
 //  Please see License for details. This notice may not be removed from this file.
--- samples/NativeCatalog/ios/InstantExample/WebExamplesAPIClient.swift
+++ samples/NativeCatalog/ios/InstantExample/WebExamplesAPIClient.swift
@@ -1,5 +1,5 @@
 //
-//  Copyright © 2017-2020 PSPDFKit GmbH. All rights reserved.
+//  Copyright © 2017-2021 PSPDFKit GmbH. All rights reserved.
 //
 //  The PSPDFKit Sample applications are licensed with a modified BSD license.
 //  Please see License for details. This notice may not be removed from this file.
