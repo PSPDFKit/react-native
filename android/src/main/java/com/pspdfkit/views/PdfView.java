6691835eb870cc0fee45b4c39643ef3f557000d1
Johannes du Plessis <johannesduplessis117@gmail.com>
2024-01-30 16:47:17 +1100

    added hashset import

--- android/src/main/java/com/pspdfkit/views/PdfView.java
+++ android/src/main/java/com/pspdfkit/views/PdfView.java
@@ -86,6 +86,7 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.NoSuchElementException;
 import java.util.concurrent.Callable;
