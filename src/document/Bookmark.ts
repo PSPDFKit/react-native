d39726140383c8ecaed171298e82b10ae51aa48b
PSPDFKit <support@pspdfkit.com>
2025-07-12 02:01:30 +0200

    Nightly 2025-07-12

--- src/document/Bookmark.ts
+++ src/document/Bookmark.ts
@@ -1,5 +1,11 @@
 /**
+ * A Bookmark is a named location in a document.
  * @interface Bookmark
+ * @memberof Bookmark
+ * @property {string} [name] - The bookmark name. This is optional.
+ * @property {string} displayName - The bookmark display name.
+ * @property {string} identifier - A string uniquely identifying the bookmark.
+ * @property {number} pageIndex - The page index on which the bookmark is located.
  */
 export class Bookmark {
     /**
